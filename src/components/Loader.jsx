8e938b9bead8cadd78b04b26d8bb6487642f0b7c
AleksandarKacar <kacar95@gmail.com>
2025-05-19 15:43:06 +0200

    Loader bug fixed

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,7 +1,6 @@
 import { RotateLoader } from "react-spinners";
 
 export default function Loader({ isLoading }) {
-    useSearchParams
   return (
     <div className="loader-container">
       <RotateLoader color={"#cc4397"} loading={isLoading} />
