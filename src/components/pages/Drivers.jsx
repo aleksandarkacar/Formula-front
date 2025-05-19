6153dfc04e82f2dc553cb86ad0890472b9661b96
DabtheGreat <thegreatdab8@gmail.com>
2025-05-19 13:57:09 +0200

    added functionality to the Drivers page

--- src/components/pages/Drivers.jsx
+++ src/components/pages/Drivers.jsx
@@ -1,5 +1,46 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Loader from "../Loader"
+
 export default function Drivers() {
+    const [drivers, setDrivers] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+
+    useEffect(() => {
+        getDrivers();
+    }, []);
+
+    const getDrivers = async () => {
+        const url = "http://ergast.com/api/f1/2013/driverStandings.json"
+        const response = await axios.get(url);
+        const data = response.data.MRData.StandingsTable.StandingsLists[0].DriverStandings;
+        setDrivers(data);
+        setIsLoading(false);
+    }
+
+    if (isLoading) {
+        return <Loader />
+    }
+
     return (
-        <h1>Drivers page</h1>
+        <div>
+            <h1>Drivers page</h1>
+            <span>Drivers Championship Standings 2013</span>
+            <table>
+                <tbody className="a">
+                    {drivers.map((driver, i) => {
+                        return (
+                            <tr key={driver.Driver.driverId}>
+                                <td>{driver.position}</td>
+                                <td>{driver.Driver.givenName} {driver.Driver.familyName}</td>
+                                <td>{driver.Constructors[0].name}</td>
+                                <td>{driver.points}</td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+        </div>
     )
 }
