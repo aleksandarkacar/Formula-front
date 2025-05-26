658b89e3c0108181a03c6b9ec7c66bf666757f8a
DabtheGreat <thegreatdab8@gmail.com>
2025-05-26 15:17:25 +0200

    edited footer/slight change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <div className="footer">
       <div className="footdiv1">
         {" "}
-        &copy; 2013 F1 RacingDashboard.All rights reserved.{" "}
+        &copy; 2013 F1 RacingDashboard. All rights reserved.{" "}
       </div>
       <div className="footdiv">Power by Ergast API</div>
     </div>
