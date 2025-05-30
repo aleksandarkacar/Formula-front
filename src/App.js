0ea67e94dfb5ef255a708436426d389bcee8b691
AleksandarKacar <kacar95@gmail.com>
2025-05-30 09:15:28 +0200

    Moved router to index.js so that use Location works

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, NavLink } from "react-router";
+import { Routes, Route, NavLink, useLocation } from "react-router";
 import Drivers from "./components/pages/Drivers";
 import DriverDetails from "./components/pages/DriverDetails";
 import Teams from "./components/pages/Teams";
@@ -9,31 +9,23 @@ import Landing from "./components/pages/Landing";
 import axios from "axios";
 import { useEffect, useRef, useState } from "react";
 import Footer from "./components/Footer";
-// import "./styles/reset.css";
-// import "./styles/App.scss";
-// import "./styles/components/nav.scss";
-// import "./styles/components/tables.scss";
 
-// import { FlagOutlined } from '@ant-design/icons';
 import { Flag } from "lucide-react";
-// import { TeamOutlined } from '@ant-design/icons';
+
 import { UsersRound } from "lucide-react";
-// import { CalendarOutlined } from '@ant-design/icons';
+
 import { Calendar } from "lucide-react";
-// import { FontSizeOutlined } from '@ant-design/icons';
+
 import { Building2 } from "lucide-react";
-import { Trophy } from "lucide-react";
 import { Search } from "lucide-react";
 
 export default function App() {
-  // console.log(currentYear);
   const [selectedYear, setSelectedYear] = useState(2024);
   const [countryList, setCountryList] = useState([]);
   const [searchInput, setSearchInput] = useState("");
   const [showInput, setShowInput] = useState(false);
   const inputRef = useRef(null);
-
-  console.log("searchInput", searchInput);
+  const location = useLocation();
 
   useEffect(() => {
     if (showInput && inputRef.current) {
@@ -61,25 +53,9 @@ export default function App() {
   console.log("selectedYear", selectedYear);
 
   return (
-    <Router>
-      {/* Navigacija */}
+    <>
       <nav className="top-navigation icons icon">
         <div className="nav-links">
-          {/* <div className="seasons">
-            <Trophy />
-            <select
-              onChange={(e) => setSelectedYear(e.target.value)}
-              value={selectedYear}
-            >
-              {allYears.map((year) => {
-                return (
-                  <option key={year} value={year}>
-                    {year}
-                  </option>
-                );
-              })}
-            </select>
-          </div> */}
           <div className="li-el">
             <NavLink to="/drivers">
               <UsersRound className="icons icon" /> Drivers
@@ -95,8 +71,12 @@ export default function App() {
               <Calendar className="icons icon" /> Races
             </NavLink>
           </div>
-          <div className="li-el search-button" onClick={handleShowInput}>
-            <Search className="icons largeIcon" />
+          <div>
+            {location.pathname !== "/" && (
+              <div className="li-el search-button" onClick={handleShowInput}>
+                <Search className="icons largeIcon" />
+              </div>
+            )}
           </div>
           {showInput && (
             <input
@@ -116,12 +96,8 @@ export default function App() {
           </NavLink>
         </div>
       </nav>
-
-      {/* Rute */}
       <div className="page">
-        {/* <div style={{backgroundColor: '#ccc'}}> */}
         <Routes className="page">
-          {/* <Route path="/" element={<Navigate to="/drivers"></Navigate>} /> */}
           <Route
             path="/drivers"
             element={
@@ -186,11 +162,9 @@ export default function App() {
             }
           />
           <Route path="/" element={<Landing to="/" />} />
-          {/* {window.location.pathname !== "/landing" ? <Footer /> : null} */}
         </Routes>
       </div>
-
       <Footer />
-    </Router>
+    </>
   );
 }
