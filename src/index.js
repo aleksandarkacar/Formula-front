aac476a7805059ca5ba4f319a2f7b81333e7dd5e
AleksandarKacar <kacar95@gmail.com>
2025-05-30 09:52:24 +0200

    Changed basename on router

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import "./styles/styles.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/Formula-front">
       <App />
     </Router>
   </React.StrictMode>
